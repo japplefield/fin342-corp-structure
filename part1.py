164c29b39d0b6cb37727a0aa103c24b5974a8f06
Justin Applefield <jmapplef@gmail.com>
2020-12-04 14:33:23 -0500

    Read in Prices into SQL

--- part1.py
+++ part1.py
@@ -66,11 +66,11 @@ def read_ebitda(cur):
 def read_debt(cur):
     cur.execute("CREATE TABLE debt"
                 "(symbol TEXT UNIQUE, "
-                 "q319 FLOAT DEFAULT NULL, "
-                 "q419 FLOAT DEFAULT NULL, "
-                 "q120 FLOAT DEFAULT NULL, "
-                 "q220 FLOAT DEFAULT NULL, "
-                 "q320 FLOAT DEFAULT NULL); ")
+                 "Q32019 FLOAT DEFAULT NULL, "
+                 "Q42019 FLOAT DEFAULT NULL, "
+                 "Q12020 FLOAT DEFAULT NULL, "
+                 "Q22020 FLOAT DEFAULT NULL, "
+                 "Q32020 FLOAT DEFAULT NULL); ")
 
     with open('SP1500Debt.csv', newline='', encoding='utf-8-sig') as ifh:
         reader = csv.DictReader(ifh)
@@ -79,159 +79,106 @@ def read_debt(cur):
             for i in range(len(vals)):
                 if vals[i] == '' or vals[i] == '#N/A':
                     vals[i] = None
-            cur.execute("INSERT INTO debt(symbol, q319, q419, q120, q220, q320) "
+            cur.execute("INSERT INTO debt(symbol, Q32019, Q42019, Q12020, Q22020, Q32020) "
                         "VALUES (?, ?, ?, ?, ?, ?)", vals)
 
 
     cur.execute("CREATE TABLE debt_cng"
                 "(symbol TEXT UNIQUE, "
-                "q419_cng FLOAT DEFAULT NULL, "
-                "q120_cng FLOAT DEFAULT NULL, "
-                "q220_cng FLOAT DEFAULT NULL, "
-                "q320_cng FLOAT DEFAULT NULL);")
+                "Q42019 FLOAT DEFAULT NULL, "
+                "Q12020 FLOAT DEFAULT NULL, "
+                "Q22020 FLOAT DEFAULT NULL, "
+                "Q32020 FLOAT DEFAULT NULL);")
 
     cur.execute("SELECT * from debt")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
         if None in row.values():
             continue
-        cur.execute("INSERT INTO debt_cng(symbol, q419_cng, q120_cng, q220_cng, q320_cng) "
-                    "VALUES (?, ?, ?, ?, ?)", [row['symbol'], row['q419'] - row['q319'], row['q120'] - row['q419'], row['q220'] - row['q120'], row['q320'] - row['q220']])
+        cur.execute("INSERT INTO debt_cng(symbol, Q42019, Q12020, Q22020, Q32020) "
+                    "VALUES (?, ?, ?, ?, ?)", [row['symbol'], row['Q42019'] - row['Q32019'], row['Q12020'] - row['Q42019'], row['Q22020'] - row['Q12020'], row['Q32020'] - row['Q22020']])
 
 # Calculate Cng Debt / Normalized EBITDA for the 4 periods
 def cng_dbt_ebd(cur):
     cur.execute("CREATE TABLE debt_cng_ebd"
                 "(symbol TEXT UNIQUE, "
-                "q419_debt_cng_ebd FLOAT DEFAULT NULL, "
-                "q120_debt_cng_ebd FLOAT DEFAULT NULL, "
-                "q220_debt_cng_ebd FLOAT DEFAULT NULL, "
-                "q320_debt_cng_ebd FLOAT DEFAULT NULL);")
+                "Q42019 FLOAT DEFAULT NULL, "
+                "Q12020 FLOAT DEFAULT NULL, "
+                "Q22020 FLOAT DEFAULT NULL, "
+                "Q32020 FLOAT DEFAULT NULL);")
 
     cur.execute("SELECT * FROM debt_cng INNER JOIN ebitda ON debt_cng.symbol=ebitda.symbol")
     rows = cur.fetchall()
     for row in rows:
-        cur.execute("INSERT INTO debt_cng_ebd(symbol, q419_debt_cng_ebd, q120_debt_cng_ebd, q220_debt_cng_ebd, q320_debt_cng_ebd) "
-                    "VALUES (?, ?, ?, ?, ?)", [row['symbol'], row['q419_cng'] / row['mean_ebd'], row['q120_cng'] / row['mean_ebd'], row['q220_cng'] / row['mean_ebd'], row['q320_cng'] / row['mean_ebd']])
+        cur.execute("INSERT INTO debt_cng_ebd(symbol, Q42019, Q12020, Q22020, Q32020) "
+                    "VALUES (?, ?, ?, ?, ?)", [row['symbol'], row['Q42019'] / row['mean_ebd'], row['Q12020'] / row['mean_ebd'], row['Q22020'] / row['mean_ebd'], row['Q32020'] / row['mean_ebd']])
 
 
+#Calculate Average price for each quarter for each firm
+def read_prices(cur):
+    Q419ST = datetime.strptime("10/01/2019", "%m/%d/%Y")
+    Q120ST = datetime.strptime("01/01/2020", "%m/%d/%Y")
+    Q220ST = datetime.strptime("04/01/2020", "%m/%d/%Y")
+    Q320ST = datetime.strptime("07/01/2020", "%m/%d/%Y")
+    Q420ST = datetime.strptime("10/01/2020", "%m/%d/%Y")
 
+    # cur.execute("CREATE TABLE avg_price"
+    #             "(symbol TEXT UNIQUE, "
+    #             "Q32019 FLOAT DEFAULT NULL, "
+    #             "Q42019 FLOAT DEFAULT NULL, "
+    #             "Q12020 FLOAT DEFAULT NULL, "
+    #             "Q22020 FLOAT DEFAULT NULL, "
+    #             "Q32020 FLOAT DEFAULT NULL);")
 
+    with open('SP1500DatedPrices.csv', newline='', encoding='utf-8-sig') as ifh:
+        reader = csv.DictReader(ifh)
+        for row in reader:
+            if '#N/A' in row.values():
+                continue
+            ticker = row['Symbol']
+            Q319AvgP = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q419ST ])
+            Q419AvgP = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q120ST ])
+            Q120AvgP = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q220ST ])
+            Q220AvgP = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q320ST ])
+            Q320AvgP = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q420ST ])
+            cur.execute("INSERT INTO avg_price(symbol, Q32019, Q42019, Q12020, Q22020, Q32020) "
+                        "VALUES (?, ?, ?, ?, ?, ?)", [ticker, Q319AvgP, Q419AvgP, Q120AvgP, Q220AvgP, Q320AvgP])
 
 
 data_dict = {}
 summary = {}
-unique_sectors = set()
-unique_industry_groups = set()
-unique_industries = set()
-unique_sub_industries = set()
-
-
 
 # read_gics(cur)
+# Get GICS Unique sectors, industry groups, industries, and sub industries for later
+cur.execute("SELECT DISTINCT sector FROM gics")
+rows = cur.fetchall()
+unique_sectors = [dct['sector'] for dct in rows]
+
+cur.execute("SELECT DISTINCT industry_group FROM gics")
+rows = cur.fetchall()
+unique_industry_groups = [dct['industry_group'] for dct in rows]
+
+cur.execute("SELECT DISTINCT industry FROM gics")
+rows = cur.fetchall()
+unique_industries = [dct['industry'] for dct in rows]
+
+cur.execute("SELECT DISTINCT sub_industry FROM gics")
+rows = cur.fetchall()
+unique_sub_industries = [dct['sub_industry'] for dct in rows]
 # read_ebitda(cur)
 # read_debt(cur)
 # cng_dbt_ebd(cur)
-con.commit()
-exit()
-
-
-
-# Calculate Debt / Normialized EBITDA for each company for each of the 5 quarters and Cng Debt / Normalized EBITDA for the 4 periods
-with open('SP1500Debt.csv', newline='', encoding='utf-8-sig') as ifH:
-    reader = csv.DictReader(ifH)
-    for row in reader:
-        try:
-            ticker = row['Symbol']
-            data_dict[ticker]['Q3 2019 Debt/EBITDA'] = float(row['Q3 2019 Debt']) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q4 2019 Debt/EBITDA'] = float(row['Q4 2019 Debt']) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q4 2019 Debt Cng/EBITDA'] = (float(row['Q4 2019 Debt']) - float(row['Q3 2019 Debt'])) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q1 2020 Debt/EBITDA'] = float(row['Q1 2020 Debt']) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q1 2020 Debt Cng/EBITDA'] = (float(row['Q1 2020 Debt']) - float(row['Q4 2019 Debt'])) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q2 2020 Debt/EBITDA'] = float(row['Q2 2020 Debt']) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q2 2020 Debt Cng/EBITDA'] = (float(row['Q2 2020 Debt']) - float(row['Q1 2020 Debt'])) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q3 2020 Debt/EBITDA'] = float(row['Q3 2020 Debt']) / data_dict[ticker]['EBITDA']
-            data_dict[ticker]['Q3 2020 Debt Cng/EBITDA'] = (float(row['Q3 2020 Debt']) - float(row['Q2 2020 Debt'])) / data_dict[ticker]['EBITDA']
-        except KeyError:
-            pass
-        except ValueError:
-            pass
-
-# Calcuate Summary Median Debt/EBITDA for each Sector
-med_debt_ebitda = {}
-for sector in unique_sectors:
-    med_debt_ebitda[sector] = {}
-    med_debt_ebitda[sector]['Q3 2019'] = statistics.median([data_dict[ticker]['Q3 2019 Debt/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q3 2019 Debt/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda[sector]['Q4 2019'] = statistics.median([data_dict[ticker]['Q4 2019 Debt/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q4 2019 Debt/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda[sector]['Q1 2020'] = statistics.median([data_dict[ticker]['Q1 2020 Debt/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q1 2020 Debt/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda[sector]['Q2 2020'] = statistics.median([data_dict[ticker]['Q2 2020 Debt/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q2 2020 Debt/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda[sector]['Q3 2020'] = statistics.median([data_dict[ticker]['Q3 2020 Debt/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q3 2020 Debt/EBITDA' in data_dict[ticker]])
-
-#Print Debt Change Line Graph by GICS Sector
-quarters = ('Q3 2019','Q4 2019', 'Q1 2020', 'Q2 2020', 'Q3 2020')
-NUM_COLORS = len(unique_sectors)
-cm = plt.get_cmap('gist_rainbow')
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_prop_cycle('color', [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
-plt.xticks(numpy.arange(5), quarters)
-plt.ylim(1.5, 4.5)
-for sector in unique_sectors:
-    plt.plot(range(5), [med_debt_ebitda[sector][quarter] for quarter in quarters],  label=sector)
-plt.xlabel('Quarter')
-plt.ylabel('Median Debt/Normalized EBITDA')
-plt.title('Median Debt/Normalized EBITDA by Sector')
-# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-plt.tight_layout()
-labelLines(plt.gca().get_lines())
-plt.savefig('med_debt_ebitda.png')
-
+# read_prices(cur)
 
 # Calcuate Summary Median Debt Cng/EBITDA for each Sector
-med_debt_ebitda_cng = {}
-for sector in unique_sectors:
-    med_debt_ebitda_cng[sector] = {}
-    med_debt_ebitda_cng[sector]['Q4 2019'] = statistics.median([data_dict[ticker]['Q4 2019 Debt Cng/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q4 2019 Debt Cng/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda_cng[sector]['Q1 2020'] = statistics.median([data_dict[ticker]['Q1 2020 Debt Cng/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q1 2020 Debt Cng/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda_cng[sector]['Q2 2020'] = statistics.median([data_dict[ticker]['Q2 2020 Debt Cng/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q2 2020 Debt Cng/EBITDA' in data_dict[ticker]])
-    med_debt_ebitda_cng[sector]['Q3 2020'] = statistics.median([data_dict[ticker]['Q3 2020 Debt Cng/EBITDA'] for ticker in data_dict if data_dict[ticker]['Sector'] == sector and 'Q3 2020 Debt Cng/EBITDA' in data_dict[ticker]])
+quarters = ('Q42019', 'Q12020', 'Q22020', 'Q32020')
+cur.execute("SELECT * FROM debt_cng_ebd INNER JOIN gics ON debt_cng_ebd.symbol=gics.symbol")
+rows = cur.fetchall()
+med_sector_dbt_ebd_cng = {sector: {quarter: statistics.median([row[quarter] for row in rows]) for quarter in quarters} for sector in unique_sectors}
 
-#Print Debt Change Line Graph by GICS Sector
-quarters = ('Q4 2019', 'Q1 2020', 'Q2 2020', 'Q3 2020')
-NUM_COLORS = len(unique_sectors)
-cm = plt.get_cmap('gist_rainbow')
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_prop_cycle('color', [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
-plt.xticks(numpy.arange(4), quarters)
-plt.ylim(-0.25, 0.5)
-for sector in unique_sectors:
-    plt.plot(range(4), [med_debt_ebitda_cng[sector][quarter] for quarter in quarters],  label=sector)
-plt.xlabel('Quarter')
-plt.ylabel('Median Debt Change/Normalized EBITDA')
-plt.title('Median Debt Change/Normalized EBITDA by Sector')
-# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-plt.tight_layout()
-labelLines(plt.gca().get_lines())
-plt.savefig('med_debt_cng_ebitda.png')
 
-#Calculate Average price for each quarter for each firm
-Q419ST = datetime.strptime("10/01/2019", "%m/%d/%Y")
-Q120ST = datetime.strptime("01/01/2020", "%m/%d/%Y")
-Q220ST = datetime.strptime("04/01/2020", "%m/%d/%Y")
-Q320ST = datetime.strptime("07/01/2020", "%m/%d/%Y")
-Q420ST = datetime.strptime("10/01/2020", "%m/%d/%Y")
-
-with open('SP1500DatedPrices.csv', newline='', encoding='utf-8-sig') as ifh:
-    reader = csv.DictReader(ifh)
-    for row in reader:
-        ticker = row['Symbol']
-        if ticker in data_dict:
-            data_dict[ticker]['Q319AvgP'] = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q419ST ])
-            data_dict[ticker]['Q419AvgP'] = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q120ST ])
-            data_dict[ticker]['Q120AvgP'] = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q220ST ])
-            data_dict[ticker]['Q220AvgP'] = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q320ST ])
-            data_dict[ticker]['Q320AvgP'] = statistics.mean([ float(row[date]) for date in reader.fieldnames[1:]  if datetime.strptime(date, "%m/%d/%Y") < Q420ST ])
+con.commit()
+exit()
 
 #Calculate change in equity value from share issuance (buybacks)
 with open('SP1500SharesOutstanding.csv', newline='', encoding='utf-8-sig') as ifh:
