8cab4060779487d79563650a11085502c75c7cd2
Justin Applefield <jmapplef@gmail.com>
2020-12-05 21:31:32 -0500

    Separate graphs for divs, shares, and tot

--- debt_bar_charts.py
+++ debt_bar_charts.py
@@ -19,16 +19,16 @@ unique_sectors = [dct['sector'] for dct in rows]
 meds = {}
 # Calculate Summary Median Eq Cng / EBITDA for each sector
 for sector in unique_sectors:
-    cur.execute("SELECT * FROM eq_cng_tot_ebd_2 INNER JOIN gics ON eq_cng_tot_ebd_2.symbol=gics.symbol WHERE gics.sector=?", [sector])
+    cur.execute("SELECT * FROM debt_cng_ebd INNER JOIN gics ON debt_cng_ebd.symbol=gics.symbol WHERE gics.sector=?", [sector])
     rows = cur.fetchall()
     meds[sector] = {quarter: statistics.median([row[quarter] for row in rows if row[quarter] is not None]) for quarter in model.quarters}
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.bar(numpy.arange(len(meds[sector].values())), meds[sector].values(), align='center')
-    plt.ylabel('Median Equity Change / Normalized EBITDA')
-    ax.text(0.5, -0.05, f'Median Quarterly Equity Change / Normalized EBITDA for {sector}', ha="center", va="top", transform=ax.transAxes)
+    plt.ylabel('Median Debt Change / Normalized EBITDA')
+    ax.text(0.5, -0.05, f'Median Quarterly Debt Change / Normalized EBITDA for {sector}', ha="center", va="top", transform=ax.transAxes)
     plt.xticks(numpy.arange(len(meds[sector].values())), model.quarters)
-    plt.ylim(-0.1, 0.015)
+    plt.ylim(-0.1, 0.5)
     ax = plt.gca()
     ax.set_xticklabels([])
     ax.spines["bottom"].set_position(("data", 0))
@@ -41,7 +41,7 @@ for sector in unique_sectors:
         else:
             label_y = y_position - label_offset
         ax.text(x_position, label_y, quarter, ha="center", va="top")
-    plt.savefig(f'Equity Bar Charts/med_eq_cng_ebitda_{sector}.png')
+    plt.savefig(f'Debt Bar Charts/med_debt_cng_ebitda_{sector}.png')
 
 
 # Close Database
@@ -55,10 +55,10 @@ q4 = ax.bar(x - 1.5*width, [meds[sector][model.quarters[0]] for sector in meds],
 q1 = ax.bar(x - 0.5*width, [meds[sector][model.quarters[1]] for sector in meds], width, label=model.quarters[1])
 q2 = ax.bar(x + 0.5*width, [meds[sector][model.quarters[2]] for sector in meds], width, label=model.quarters[2])
 q3 = ax.bar(x + 1.5*width, [meds[sector][model.quarters[3]] for sector in meds], width, label=model.quarters[3])
-
-ax.set_ylabel('Median Equity Change / EBITDA')
-ax.set_title('Median Equity Change / EBITDA by Sector, Last 4 Quarters')
+ax.axhline(color='black')
+ax.set_ylabel('Median Debt Change / EBITDA')
+ax.set_title('Median Debt Change / EBITDA by Sector, Last 4 Quarters')
 ax.set_xticks(x)
 ax.set_xticklabels(unique_sectors)
 ax.legend()
-plt.savefig('Equity Bar Charts/med_eq_cng_ebitda_all.png')
+plt.savefig('Debt Bar Charts/med_debt_cng_ebitda_all.png')
---
+++ equity_divs_bar_charts.py
@@ -0,0 +1,64 @@
+import model
+import statistics
+import matplotlib.pyplot as plt
+import numpy
+from matplotlib import rcParams
+from labellines import labelLine, labelLines
+
+rcParams['figure.figsize'] = [10, 10]
+
+con = model.sql_connection()
+cur = con.cursor()
+
+# Get GICS Unique sectors
+cur.execute("SELECT DISTINCT sector FROM gics")
+rows = cur.fetchall()
+unique_sectors = [dct['sector'] for dct in rows]
+
+
+meds = {}
+# Calculate Summary Median Eq Cng / EBITDA for each sector
+for sector in unique_sectors:
+    cur.execute("SELECT * FROM eq_cng_divs_ebd_2 INNER JOIN gics ON eq_cng_divs_ebd_2.symbol=gics.symbol WHERE gics.sector=?", [sector])
+    rows = cur.fetchall()
+    meds[sector] = {quarter: statistics.median([row[quarter] for row in rows if row[quarter] is not None]) for quarter in model.quarters}
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.bar(numpy.arange(len(meds[sector].values())), meds[sector].values(), align='center')
+    plt.ylabel('Median Equity Change Attributable to Dividends / Normalized EBITDA')
+    ax.text(0.5, -0.05, f'Median Quarterly Equity Change Attributable to Dividends / Normalized EBITDA for {sector}', ha="center", va="top", transform=ax.transAxes)
+    plt.xticks(numpy.arange(len(meds[sector].values())), model.quarters)
+    plt.ylim(-0.16, 0)
+    ax = plt.gca()
+    ax.set_xticklabels([])
+    ax.spines["bottom"].set_position(("data", 0))
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    label_offset = 0.005
+    for quarter, (x_position, y_position) in zip(model.quarters, enumerate(meds[sector].values())):
+        if y_position > 0:
+            label_y = -label_offset
+        else:
+            label_y = y_position - label_offset
+        ax.text(x_position, label_y, quarter, ha="center", va="top")
+    plt.savefig(f'Equity Bar Charts/med_eq_divs_cng_ebitda_{sector}.png')
+
+
+# Close Database
+con.commit()
+
+rcParams['figure.figsize'] = [25, 10]
+x = numpy.arange(len(unique_sectors))
+width = 0.2
+fig, ax = plt.subplots()
+q4 = ax.bar(x - 1.5*width, [meds[sector][model.quarters[0]] for sector in meds], width, label=model.quarters[0])
+q1 = ax.bar(x - 0.5*width, [meds[sector][model.quarters[1]] for sector in meds], width, label=model.quarters[1])
+q2 = ax.bar(x + 0.5*width, [meds[sector][model.quarters[2]] for sector in meds], width, label=model.quarters[2])
+q3 = ax.bar(x + 1.5*width, [meds[sector][model.quarters[3]] for sector in meds], width, label=model.quarters[3])
+ax.axhline(color='black')
+ax.set_ylabel('Median Equity Change Attributable to Dividends/ EBITDA')
+ax.set_title('Median Equity Change Attributable to Dividends / EBITDA by Sector, Last 4 Quarters')
+ax.set_xticks(x)
+ax.set_xticklabels(unique_sectors)
+ax.legend()
+plt.savefig('Equity Bar Charts/med_eq_divs_cng_ebitda_all.png')
